021fb5a2f689d72fd8a5a50c43d6b9b08fa1385c
Ethan Xu <ethan.xu.3.1415@gmail.com>
2023-09-08 22:58:54 -0700

    added ssbo

--- raytracer/texture.cpp
+++
@@ -1,9 +0,0 @@
-//#include "texture.h"
-//
-//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <sstream>
-//#include "stb/stb_image.h"
